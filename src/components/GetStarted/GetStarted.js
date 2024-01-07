57fb88ae4e9147dd39b10d9e1d94efd54eb278e6
Shiv kumar <103626079+11shiv@users.noreply.github.com>
2024-01-07 19:19:18 +0530

    Add files via upload

---
+++ src/components/GetStarted/GetStarted.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './GetStarted.css';
+
+
+const GetStarted = () => {
+  return (
+    <section className='g-wrapper'>
+      <div className='paddings innerwidth g-container'>
+        <div className='flexColCenter inner-container'>
+          <span className='primaryText'>GetStarted with Homyz</span>
+          <span className='secondaryText'>
+            Subscribe and find super attractive price quotes from
+            <br/>
+            find your Residence soon 
+          </span>
+          <button className='button'>
+          <a href='shiv11study.in@gmail.com'>Get Started</a>
+          </button>
+        </div>
+      </div>
+
+    </section>
+    
+  )
+}
+
+export default GetStarted
